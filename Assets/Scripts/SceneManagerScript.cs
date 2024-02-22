b3404aaeaec1260a91e81ca0c5f270c03b6f01d6
Olivia <104592012+OliviaStarStuff@users.noreply.github.com>
2024-02-22 09:44:40 +0000

    Add art assets to startPage scene

--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -8,4 +8,9 @@ public class SceneManagerScript : MonoBehaviour
     public static void LoadScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
     }
+
+    public static void Quit() {
+        Debug.Log("Quitting Application");
+        Application.Quit();
+    }
 }
