c86517e658f62101b6ddce5b5b4528020b7cc87f
Olivia <104592012+OliviaStarStuff@users.noreply.github.com>
2024-02-22 10:08:52 +0000

    Add editor quit code to script

--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -11,6 +11,12 @@ public class SceneManagerScript : MonoBehaviour
 
     public static void Quit() {
         Debug.Log("Quitting Application");
-        Application.Quit();
+        #if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
