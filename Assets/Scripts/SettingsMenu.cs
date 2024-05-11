31c87d96732979b92e1b0c0566f3a0bd76b3f175
AbdallahKayed <124938490+AbdallahKayed@users.noreply.github.com>
2024-05-11 21:24:37 +0100

    [feat]: Create the settings and resume buttons and add them to the level selector and settings scenes

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -33,5 +34,10 @@ public class SettingsMenu : MonoBehaviour
     {
         float SfxVolume = SfxSlider.value;
         SoundVolume.SetFloat("SfxVolume", SfxVolume);
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(sceneName: "LevelSelection");
     }
 }
