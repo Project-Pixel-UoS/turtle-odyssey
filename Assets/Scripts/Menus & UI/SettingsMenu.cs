3aa84a03213f50f6c521c0bc169822253281e7a5
Olivia <104592012+OliviaStarStuff@users.noreply.github.com>
2024-11-12 12:07:27 +0000

    [feat]: fix audio setting

--- Assets/Scripts/Menus & UI/SettingsMenu.cs
+++ Assets/Scripts/Menus & UI/SettingsMenu.cs
@@ -20,19 +20,26 @@ public class SettingsMenu : MonoBehaviour
 
     private void Start()
     {
-        MusicVolumeSet();
-        SfxVolumeSet();
+        // MusicVolumeSet();
+        float musicVolume, sfxVolume;
+        SoundVolume.GetFloat("MusicVolume", out musicVolume);
+        SoundVolume.GetFloat("SfxVolume", out sfxVolume);
+        VolumeSlider.value = Mathf.Pow(10f, (musicVolume / 20.0f));
+        SfxSlider.value = Mathf.Pow(10f, (sfxVolume/ 20.0f));
+        // SfxVolumeSet();
     }
 
     public void MusicVolumeSet()
     {
-        float Volume = VolumeSlider.value;
+        float Volume = Mathf.Log10(VolumeSlider.value) * 20;
+        if (VolumeSlider.value == 0.0) {Volume = -80f;}
         SoundVolume.SetFloat("MusicVolume", Volume);
     }
 
     public void SfxVolumeSet()
     {
-        float SfxVolume = SfxSlider.value;
+        float SfxVolume = Mathf.Log10(SfxSlider.value) * 20;
+        if (SfxSlider.value == 0.0) {SfxVolume = -80f;}
         SoundVolume.SetFloat("SfxVolume", SfxVolume);
     }
 
